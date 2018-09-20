6a3c0a087aa2394c3872ab1d2477aaa30c2389fc
wyyx <gnehcyx@163.com>
2018-09-20 18:59:04 +0800

    fix proxyTable config

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.get('*', (req, res) => {
 })
 
 // start app
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 5000
 app.listen(PORT, err => {
 	if (err) {
 		console.log(err)
